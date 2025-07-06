677311d25a6e02425e924cfcc4f2b3020f29b895
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2025-07-06 19:59:01 +0900

    feat: implement task switch

--- src/boot/vector_tbl.c
+++ src/boot/vector_tbl.c
@@ -8,6 +8,7 @@
 #include <syslib.h>
 #include <typedef.h>
 
+extern void dispatch_entry(void); /* ディスパッチャ */
 /* デフォルトハンドラ */
 void Default_Handler(void) { while (1); }
 
@@ -27,7 +28,7 @@ void (*const vector_tbl[])() __attribute__((section(".vector"))) = {
     Default_Handler,           // 11: Svcall
     0,                         // 12: 未使用
     0,                         // 13: 未使用
-    Default_Handler,           // 14: Pend SV
+    dispatch_entry,            // 14: Pend SV
     Default_Handler,           // 15: Systick
     Default_Handler,           // IRQ 0
     Default_Handler,           // IRQ 1
