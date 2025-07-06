73f07f633ea908998470241a666a769cc730d3c4
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2025-07-06 20:36:44 +0900

    feat: implement semaphore

--- src/include/config.h
+++ src/include/config.h
@@ -8,4 +8,7 @@
 #define CNF_MAX_TSKID 32  /* 最大タスク数 */
 #define CNF_MAX_TSKPRI 16 /* 最大タスク優先度 */
 
+#define CNF_MAX_FLGID 8 /* 最大イベントフラグ数 */
+#define CNF_MAX_SEMID 8 /* 最大セマフォ数 */
+
 #endif /* CONFIG_H */
